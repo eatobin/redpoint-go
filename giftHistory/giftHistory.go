4f64b7c3b290ef74a77d52e940a029a9f313f910
Eric Tobin <eatobin@gmail.com>
2021-09-09 15:02:29 -0500

    pointers progress

--- giftHistory/giftHistory.go
+++ giftHistory/giftHistory.go
@@ -3,13 +3,14 @@ package giftHistory
 import "github.com/eatobin/redpointGo/giftPair"
 
 type GiftPair = giftPair.GiftPair
-type GiftHistory = []GiftPair
+type GiftPairPtr = *GiftPair
+type GiftHistory = []GiftPairPtr
 
 func AddYear(playerKey string, giftHistory GiftHistory) GiftHistory {
-	return append(giftHistory, GiftPair{Givee: playerKey, Giver: playerKey})
+	return append(giftHistory, &GiftPair{Givee: playerKey, Giver: playerKey})
 }
 
-func UpdateGiftHistory(giftYear int, giftPair GiftPair, giftHistory GiftHistory) GiftHistory {
-	giftHistory[giftYear] = giftPair
-	return giftHistory
-}
+//func UpdateGiftHistory(giftYear int, giftPair GiftPair, giftHistory GiftHistory) GiftHistory {
+//	giftHistory[giftYear] = giftPair
+//	return giftHistory
+//}
--- giftHistory/giftHistory_test.go
+++ giftHistory/giftHistory_test.go
@@ -1,16 +1,17 @@
 package giftHistory
 
-import (
-	"testing"
-)
+import "testing"
 
-//var jsonStringGH = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
-var giftHistory = GiftHistory{{Givee: "GeoHar", Giver: "JohLen"}}
+////var jsonStringGH = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
+var giftPairPtr = &GiftPair{Givee: "GeoHar", Giver: "JohLen"}
+var giftHistory = GiftHistory{giftPairPtr}
 var gotGHAY = AddYear("NewBee", giftHistory)
-var gotGHUGH = UpdateGiftHistory(0, GiftPair{Givee: "me", Giver: "you"}, giftHistory)
-var giftHistoryExtended = GiftHistory{{Givee: "GeoHar", Giver: "JohLen"}, {Givee: "NewBee", Giver: "NewBee"}}
-var giftHistoryMeYou = GiftHistory{{"me", "you"}}
 
+//var gotGHUGH = UpdateGiftHistory(0, GiftPair{Givee: "me", Giver: "you"}, giftHistory)
+var giftHistoryExtended = GiftHistory{giftPairPtr, &GiftPair{Givee: "NewBee", Giver: "NewBee"}}
+
+//var giftHistoryMeYou = GiftHistory{{"me", "you"}}
+//
 // Equal tells whether a and b contain the same elements.
 // A nil argument is equivalent to an empty slice.
 func equal(a, b GiftHistory) bool {
@@ -18,7 +19,7 @@ func equal(a, b GiftHistory) bool {
 		return false
 	}
 	for i, v := range a {
-		if v != b[i] {
+		if *v != *b[i] {
 			return false
 		}
 	}
@@ -31,11 +32,11 @@ func TestAddYear(t *testing.T) {
 	}
 }
 
-//it should "return an updated giftHistory" in {
-//assert(GiftHistory.UpdateGiftHistory(0)(GiftPair("me", "you"))(giftHistory) == Vector(GiftPair("me", "you")))
+////it should "return an updated giftHistory" in {
+////assert(GiftHistory.UpdateGiftHistory(0)(GiftPair("me", "you"))(giftHistory) == Vector(GiftPair("me", "you")))
+////}
+//func TestUpdateGiftHistory(t *testing.T) {
+//	if !equal(gotGHUGH, giftHistoryMeYou) {
+//		t.Fatalf("UpdateGiftHistory(%d, %v, %v) == %v, want %v", 0, GiftPair{Givee: "me", Giver: "you"}, giftHistory, gotGHUGH, giftHistoryMeYou)
+//	}
 //}
-func TestUpdateGiftHistory(t *testing.T) {
-	if !equal(gotGHUGH, giftHistoryMeYou) {
-		t.Fatalf("UpdateGiftHistory(%d, %v, %v) == %v, want %v", 0, GiftPair{Givee: "me", Giver: "you"}, giftHistory, gotGHUGH, giftHistoryMeYou)
-	}
-}
