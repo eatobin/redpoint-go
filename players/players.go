4315172bd2214f71e68d19a1ad2f94dbc4f3b06b
Eric Tobin <eatobin@gmail.com>
2021-10-14 18:11:15 -0500

    players progress

---
+++ players/players.go
@@ -0,0 +1,17 @@
+package players
+
+import (
+	"github.com/eatobin/redpointGo/giftHistory"
+	"github.com/eatobin/redpointGo/giftPair"
+	"github.com/eatobin/redpointGo/player"
+)
+
+type GiftPair = giftPair.GiftPair
+type GiftHistory = giftHistory.GiftHistory
+type Player = player.Player
+type Players = map[string]Player
+
+func UpdatePlayer(playerKey string, player Player, players Players) Players {
+	players[playerKey] = player
+	return players
+}
