3ced5d3f401c9f2acc9f227010da74446463a591
Eric Tobin <eatobin@gmail.com>
2022-01-19 16:24:35 -0600

    players.go progress

--- players/players.go
+++ players/players.go
@@ -1,6 +1,7 @@
 package players
 
 import (
+	"encoding/json"
 	"github.com/eatobin/redpoint-go/giftHistory"
 	"github.com/eatobin/redpoint-go/giftPair"
 	"github.com/eatobin/redpoint-go/player"
@@ -50,4 +51,27 @@ func GetGivee(selfKey string, giftYear int, players Players) Givee {
 
 func GetGiver(selfKey string, giftYear int, players Players) Giver {
 	return players[selfKey].GiftHistory[giftYear].Giver
+}
+
+func setGiftPair(playerKey string, giftYear int, giftPair GiftPair, players Players) Players {
+	ngh := giftHistory.UpdateGiftHistory(giftYear, giftPair, players[playerKey].GiftHistory)
+	nplr := players[playerKey].UpdateGiftHistory(ngh)
+	return UpdatePlayer(playerKey, nplr, players)
+}
+
+func UpdateGivee(selfKey string, giftYear int, givee Givee, players Players) Players {
+	ngp := players[selfKey].GiftHistory[giftYear].UpdateGivee(givee)
+	return setGiftPair(selfKey, giftYear, ngp, players)
+}
+
+func UpdateGiver(selfKey string, giftYear int, giver Giver, players Players) Players {
+	ngp := players[selfKey].GiftHistory[giftYear].UpdateGiver(giver)
+	return setGiftPair(selfKey, giftYear, ngp, players)
+}
+
+// JsonStringToPlayers turns a Players JSON string into a Players
+func JsonStringToPlayers(ghString string) (Players, error) {
+	var players Players
+	err := json.Unmarshal([]byte(ghString), &players)
+	return players, err
 }
