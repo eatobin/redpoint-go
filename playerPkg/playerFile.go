426337e919eff19101da97cc53cbfb375a25094b
Eric Tobin <eatobin@gmail.com>
2023-03-30 12:59:00 -0700

    playerFile.go progress

--- playerPkg/playerFile.go
+++ playerPkg/playerFile.go
@@ -1,8 +1,6 @@
 package playerPkg
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/eatobin/redpoint-go/giftHistoryPkg"
 	"github.com/eatobin/redpoint-go/giftPairPkg"
 )
@@ -35,26 +33,27 @@ func ComparePlayer(a, b Player) bool {
 	return true
 }
 
-// UpdateGiftHistory updates a GiftHistory in a Player
-func (player Player) UpdateGiftHistory(gh GiftHistory) Player {
-	player.GiftHistory = gh
-	return player
-}
-
-// String makes a Player into a string
-func (player Player) String() string {
-	return fmt.Sprintf("{PlayerName: %s, GiftHistory: %v}", player.PlayerName, player.GiftHistory)
-}
-
-// JsonStringToPlayer turns a Player JSON string into a Player
-func JsonStringToPlayer(playerString string) (Player, error) {
-	var player Player
-	err := json.Unmarshal([]byte(playerString), &player)
-	return player, err
-}
-
-// PlayerToJsonString turns a Player into a Player JSON string
-func (player Player) PlayerToJsonString() (string, error) {
-	playerByte, err := json.Marshal(player)
-	return string(playerByte), err
-}
+//
+//// UpdateGiftHistory updates a GiftHistory in a Player
+//func (player Player) UpdateGiftHistory(gh GiftHistory) Player {
+//	player.GiftHistory = gh
+//	return player
+//}
+//
+//// String makes a Player into a string
+//func (player Player) String() string {
+//	return fmt.Sprintf("{PlayerName: %s, GiftHistory: %v}", player.PlayerName, player.GiftHistory)
+//}
+//
+//// JsonStringToPlayer turns a Player JSON string into a Player
+//func JsonStringToPlayer(playerString string) (Player, error) {
+//	var player Player
+//	err := json.Unmarshal([]byte(playerString), &player)
+//	return player, err
+//}
+//
+//// PlayerToJsonString turns a Player into a Player JSON string
+//func (player Player) PlayerToJsonString() (string, error) {
+//	playerByte, err := json.Marshal(player)
+//	return string(playerByte), err
+//}
--- playerPkg/playerFile_test.go
+++ playerPkg/playerFile_test.go
@@ -2,47 +2,50 @@ package playerPkg
 
 import "testing"
 
-var jsonStringPlr = "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
-var playerA = Player{PlayerName: "Paul McCartney", GiftHistory: GiftHistory{{Givee: "GeoHar", Giver: "JohLen"}}}
-var playerB = Player{PlayerName: "Paul McCartney", GiftHistory: GiftHistory{{Givee: "GeoHar", Giver: "JohLen"}}}
+//var jsonStringPlr = "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+
 var playerC = Player{PlayerName: "Paul McCartney", GiftHistory: GiftHistory{{Givee: "NotEven", Giver: "Close"}}}
 var playerD = Player{PlayerName: "Nope", GiftHistory: GiftHistory{{Givee: "GeoHar", Giver: "JohLen"}}}
 var playerE = Player{PlayerName: "Paul McCartney", GiftHistory: GiftHistory{{Givee: "GeoHar", Giver: "JohLen"}, {Givee: "NewBee", Giver: "NewBee"}}}
 
 func TestComparePlayer(t *testing.T) {
+	t.Parallel()
+	var playerA = Player{PlayerName: "Paul McCartney", GiftHistory: GiftHistory{{Givee: "GeoHar", Giver: "JohLen"}}}
+	var playerB = Player{PlayerName: "Paul McCartney", GiftHistory: GiftHistory{{Givee: "GeoHar", Giver: "JohLen"}}}
 	if !ComparePlayer(playerA, playerB) {
 		t.Fatalf("ComparePlayer(%v,\n%v) == %t, want %t", playerA, playerB, false, true)
 	}
-	if ComparePlayer(playerB, playerC) {
-		t.Fatalf("ComparePlayer(%v,\n%v) == %t, want %t", playerB, playerC, true, false)
-	}
-	if ComparePlayer(playerB, playerD) {
-		t.Fatalf("ComparePlayer(%v,\n%v) == %t, want %t", playerB, playerD, true, false)
-	}
-	if ComparePlayer(playerB, playerE) {
-		t.Fatalf("ComparePlayer(%v,\n%v) == %t, want %t", playerB, playerE, true, false)
-	}
+	//if ComparePlayer(playerB, playerC) {
+	//	t.Fatalf("ComparePlayer(%v,\n%v) == %t, want %t", playerB, playerC, true, false)
+	//}
+	//if ComparePlayer(playerB, playerD) {
+	//	t.Fatalf("ComparePlayer(%v,\n%v) == %t, want %t", playerB, playerD, true, false)
+	//}
+	//if ComparePlayer(playerB, playerE) {
+	//	t.Fatalf("ComparePlayer(%v,\n%v) == %t, want %t", playerB, playerE, true, false)
+	//}
 }
 
-func TestUpdateGiftHistory(t *testing.T) {
-	newGH := GiftHistory{{Givee: "nope", Giver: "yup"}}
-	newPlayer := Player{PlayerName: "Paul McCartney", GiftHistory: GiftHistory{{Givee: "nope", Giver: "yup"}}}.String()
-	gotPlayer := playerA.UpdateGiftHistory(newGH).String()
-	if gotPlayer != newPlayer {
-		t.Fatalf("(%v) UpdateGiftHistory(%v) ==\n%v,\nwant %v", playerA, newGH, gotPlayer, newPlayer)
-	}
-}
-
-func TestJsonStringToPlayer(t *testing.T) {
-	gotPlayer, _ := JsonStringToPlayer(jsonStringPlr)
-	if gotPlayer.String() != playerA.String() {
-		t.Fatalf("JsonStringToGiftPlayer(%v) ==\n%v,\nwant %v", jsonStringPlr, gotPlayer.String(), playerA.String())
-	}
-}
-
-func TestPlayerToJsonString(t *testing.T) {
-	gotString, _ := playerA.PlayerToJsonString()
-	if gotString != jsonStringPlr {
-		t.Fatalf("(%v) JsonStringToPlayer() ==\n%v,\nwant %v", playerA, gotString, jsonStringPlr)
-	}
-}
+//
+//func TestUpdateGiftHistory(t *testing.T) {
+//	newGH := GiftHistory{{Givee: "nope", Giver: "yup"}}
+//	newPlayer := Player{PlayerName: "Paul McCartney", GiftHistory: GiftHistory{{Givee: "nope", Giver: "yup"}}}.String()
+//	gotPlayer := playerA.UpdateGiftHistory(newGH).String()
+//	if gotPlayer != newPlayer {
+//		t.Fatalf("(%v) UpdateGiftHistory(%v) ==\n%v,\nwant %v", playerA, newGH, gotPlayer, newPlayer)
+//	}
+//}
+//
+//func TestJsonStringToPlayer(t *testing.T) {
+//	gotPlayer, _ := JsonStringToPlayer(jsonStringPlr)
+//	if gotPlayer.String() != playerA.String() {
+//		t.Fatalf("JsonStringToGiftPlayer(%v) ==\n%v,\nwant %v", jsonStringPlr, gotPlayer.String(), playerA.String())
+//	}
+//}
+//
+//func TestPlayerToJsonString(t *testing.T) {
+//	gotString, _ := playerA.PlayerToJsonString()
+//	if gotString != jsonStringPlr {
+//		t.Fatalf("(%v) JsonStringToPlayer() ==\n%v,\nwant %v", playerA, gotString, jsonStringPlr)
+//	}
+//}
