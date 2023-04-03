c1f1084f64fe4f0a44cb4323336b094d51fdf7ad
Eric Tobin <eatobin@gmail.com>
2023-04-03 11:04:50 -0700

    progress

--- player/player.go
+++ player/player.go
@@ -9,48 +9,52 @@ import (
 
 type Name = string
 
-// A Player has a PlayerName and a GiftHistory
-type Player struct {
+// A Struct has a PlayerName and a GiftHistory
+type Struct struct {
 	PlayerName  Name                `json:"playerName"`
 	GiftHistory giftHistory.History `json:"giftHistory"`
 }
 
-// JsonStringToPlayer turns a JSON string into a Player
-func JsonStringToPlayer(jsonString giftPair.JsonString) (Player, error) {
-	var player Player
+// JsonStringToPlayer turns a JSON string into a Struct
+func JsonStringToPlayer(jsonString giftPair.JsonString) (Struct, error) {
+	var player Struct
 	err := json.Unmarshal([]byte(jsonString), &player)
 	if err != nil {
-		return Player{}, err
+		return Struct{}, err
 	}
 	if player.PlayerName == "" {
 		err = errors.New("missing PlayerName field value")
-		return Player{}, err
+		return Struct{}, err
+	}
+	if len(player.GiftHistory) == 0 {
+		err = errors.New("missing GiftHistory field value")
+		return Struct{}, err
 	}
 	for _, v := range player.GiftHistory {
 		if v.Givee == "" || v.Giver == "" {
 			err = errors.New("missing a GiftHistory field value")
-			return Player{}, err
+			return Struct{}, err
 		}
 	}
 	return player, nil
 }
 
 //
-//// UpdateGiftHistory updates a GiftHistory in a Player
-//func (player Player) UpdateGiftHistory(gh GiftHistory) Player {
+//// UpdateGiftHistory updates a GiftHistory in a Struct
+//func (player Struct) UpdateGiftHistory(gh GiftHistory) Struct {
 //	player.GiftHistory = gh
 //	return player
 //}
 //
-//// String makes a Player into a string
-//func (player Player) String() string {
+//// String makes a Struct into a string
+//func (player Struct) String() string {
 //	return fmt.Sprintf("{PlayerName: %s, GiftHistory: %v}", player.PlayerName, player.GiftHistory)
 //}
 //
 
 //
-//// PlayerToJsonString turns a Player into a Player JSON string
-//func (player Player) PlayerToJsonString() (string, error) {
+//// PlayerToJsonString turns a Struct into a Struct JSON string
+//func (player Struct) PlayerToJsonString() (string, error) {
 //	playerByte, err := json.Marshal(player)
 //	return string(playerByte), err
 //}
