ab363b10bcdb7e5bd78be2450167be1bd9d04e79
Eric Tobin <eatobin@gmail.com>
2023-04-18 18:02:34 -0500

    rebuilding

---
+++ giftHistory.go
@@ -0,0 +1,37 @@
+package redpoint
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+type History []GiftPairStruct
+
+// GiftHistoryJsonStringToGiftHistory turns a JSON string into a GiftHistory
+func GiftHistoryJsonStringToGiftHistory(jsonString string) (History, error) {
+	var giftHistory History
+	err := json.Unmarshal([]byte(jsonString), &giftHistory)
+	if err != nil {
+		return History{}, err
+	}
+	for _, v := range giftHistory {
+		if v.Givee == "" || v.Giver == "" {
+			err = errors.New("missing one or both GiftPair field values somewhere")
+			return History{}, err
+		}
+	}
+	return giftHistory, nil
+}
+
+// GiftHistoryAddYear adds a playerKey to each giftHistory
+func (giftHistory History) GiftHistoryAddYear(playerKey string) History {
+	return append(giftHistory, GiftPairStruct{Givee: playerKey, Giver: playerKey})
+}
+
+// GiftHistoryUpdateGiftHistory replaces a giftPair at a given giftYear
+func (giftHistory History) GiftHistoryUpdateGiftHistory(giftYear int, giftPair GiftPairStruct) History {
+	newGiftHistory := make(History, len(giftHistory))
+	copy(newGiftHistory, giftHistory)
+	newGiftHistory[giftYear] = giftPair
+	return newGiftHistory
+}
---
+++ giftHistory_test.go
@@ -0,0 +1,95 @@
+package redpoint
+
+import (
+	"reflect"
+	"testing"
+)
+
+var giftHistory1GH = History{{Givee: "GeoHar", Giver: "JohLen"}}
+var giftHistory2GH = History{{Givee: "GeoHar", Giver: "JohLen"}, {Givee: "NewBee", Giver: "NewBee"}}
+var giftHistory3GH = History{{Givee: "me", Giver: "you"}}
+var jsonStringGH = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
+var badJsonStringGH = "[{\"givee\"\"GeoHar\",\"giver\":\"JohLen\"}]"
+var badJsonString2GH = "[{\"giveeX\":\"GeoHar\",\"giver\":\"JohLen\"}]"
+var giftPair1GH = GiftPairStruct{Givee: "me", Giver: "you"}
+
+func TestGiftHistoryJsonStringToGiftHistory(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		jsonString string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    History
+		wantErr bool
+	}{
+		{name: "ValidInput", args: args{jsonStringGH}, want: giftHistory1GH, wantErr: false},
+		{name: "InvalidInputBadJSON", args: args{badJsonStringGH}, want: History{}, wantErr: true},
+		{name: "InvalidInputBadFieldName", args: args{badJsonString2GH}, want: History{}, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GiftHistoryJsonStringToGiftHistory(tt.args.jsonString)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GiftHistoryJsonStringToGiftHistory() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GiftHistoryJsonStringToGiftHistory() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHistory_GiftHistoryAddYear(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		playerKey string
+	}
+	tests := []struct {
+		name        string
+		giftHistory History
+		args        args
+		want        History
+	}{
+		{name: "ValidInput", giftHistory: giftHistory1GH, args: args{playerKey: "NewBee"}, want: giftHistory2GH},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.giftHistory.GiftHistoryAddYear(tt.args.playerKey); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GiftHistoryAddYear() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHistory_GiftHistoryUpdateGiftHistory(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		giftYear int
+		giftPair GiftPairStruct
+	}
+	tests := []struct {
+		name        string
+		giftHistory History
+		args        args
+		want        History
+	}{
+		{
+			name:        "ValidInput",
+			giftHistory: giftHistory1GH,
+			args: args{
+				giftYear: 0,
+				giftPair: giftPair1GH,
+			},
+			want: giftHistory3GH},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.giftHistory.GiftHistoryUpdateGiftHistory(tt.args.giftYear, tt.args.giftPair); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GiftHistoryUpdateGiftHistory() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
