eec3ec84e9516211795875523d34a523b3ca3ab8
Eric Tobin <eatobin@gmail.com>
2023-04-18 20:53:56 -0500

    rebuilding

--- player_test.go
+++ player_test.go
@@ -1,24 +1,23 @@
 package redpoint
 
 import (
-	"github.com/eatobin/redpoint-go/giftHistoryPkg"
 	"reflect"
 	"testing"
 )
 
-var jsonString = "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
-var player1 = PlayerStruct{PlayerName: "Paul McCartney", GiftHistory: giftHistoryPkg.History{{Givee: "GeoHar", Giver: "JohLen"}}}
-var badJsonString = "{\"playerName\"\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
-var badJsonString2 = "{\"playerNameX\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
-var badJsonString3 = "{\"playerName\":\"Paul McCartney\",\"giftHistoryX\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
-var badJsonString4 = "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"giveeX\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+var jsonStringPlr = "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+var player1 = PlayerStruct{PlayerName: "Paul McCartney", GiftHistory: History{{Givee: "GeoHar", Giver: "JohLen"}}}
+var badJsonStringPlr = "{\"playerName\"\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+var badJsonStringPlr2 = "{\"playerNameX\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+var badJsonStringPlr3 = "{\"playerName\":\"Paul McCartney\",\"giftHistoryX\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+var badJsonStringPlr4 = "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"giveeX\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
 
 //var player2 = StructGiftPair{PlayerName: "Paul McCartney", GiftHistory: giftHistory.History{{Givee: "GeoHar", Giver: "JohLen"}}}
 //var player3 = StructGiftPair{PlayerName: "Paul McCartney", GiftHistory: giftHistory.History{{Givee: "Nope", Giver: "JohLen"}}}
 //var player4 = StructGiftPair{PlayerName: "Nope", GiftHistory: giftHistory.History{{Givee: "GeoHar", Giver: "JohLen"}}}
 //var player5 = StructGiftPair{PlayerName: "Paul McCartney", GiftHistory: giftHistory.History{{Givee: "GeoHar", Giver: "JohLen"}, {Givee: "NewBee", Giver: "NewBee"}}}
 
-func TestJsonStringToPlayer(t *testing.T) {
+func TestPlayerJsonStringToPlayer(t *testing.T) {
 	t.Parallel()
 	type args struct {
 		jsonString string
@@ -29,11 +28,11 @@ func TestJsonStringToPlayer(t *testing.T) {
 		want    PlayerStruct
 		wantErr bool
 	}{
-		{name: "ValidInput", args: args{jsonString}, want: player1, wantErr: false},
-		{name: "InvalidInputBadJSON", args: args{badJsonString}, want: PlayerStruct{}, wantErr: true},
-		{name: "InvalidInputBadPlayerName", args: args{badJsonString2}, want: PlayerStruct{}, wantErr: true},
-		{name: "InvalidInputBadHistoryName", args: args{badJsonString3}, want: PlayerStruct{}, wantErr: true},
-		{name: "InvalidInputBadHistoryField", args: args{badJsonString4}, want: PlayerStruct{}, wantErr: true},
+		{name: "ValidInput", args: args{jsonStringPlr}, want: player1, wantErr: false},
+		{name: "InvalidInputBadJSON", args: args{badJsonStringPlr}, want: PlayerStruct{}, wantErr: true},
+		{name: "InvalidInputBadPlayerName", args: args{badJsonStringPlr2}, want: PlayerStruct{}, wantErr: true},
+		{name: "InvalidInputBadHistoryName", args: args{badJsonStringPlr3}, want: PlayerStruct{}, wantErr: true},
+		{name: "InvalidInputBadHistoryField", args: args{badJsonStringPlr4}, want: PlayerStruct{}, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
